f3080715a755e8db63d05e760d6502444bf1261b
dorisxx <dorisxiang@gatech.edu>
2021-04-07 17:13:05 -0400

    fractal output red bitmap

--- Fractal/main.cpp
+++ Fractal/main.cpp
@@ -1,11 +1,24 @@
 #include <iostream>
-#include "BitmapFileHeader.h"
-#include "BitmapInfoHeader.h"
+#include "Bitmap.h"
 
+using namespace BitmapAll;
 using namespace std;
 
 int main()
 {
-    cout << "hello fractal" << endl;
+    int const WIDTH = 800;
+    int const HEIGHT = 600;
+
+    Bitmap bitmap(WIDTH, HEIGHT);
+    // make the entire bitmap red.
+    for (int y = 0; y < HEIGHT; y++)
+    {
+        for (int x = 0; x < WIDTH; x++)
+        {
+            bitmap.setPixel(x, y, 255, 0, 0);
+        }
+    }
+    bitmap.write("test.bmp");
+    cout << "Finished" << endl;
     return 0;
 }
