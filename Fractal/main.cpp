43d921e0c5501926a59a1ece5cf7cc91125bf064
dorisxx <dorisxiang@gatech.edu>
2021-04-07 21:03:26 -0400

    set color for fractal

--- Fractal/main.cpp
+++ Fractal/main.cpp
@@ -18,17 +18,18 @@ int main()
     {
         for (int x = 0; x < WIDTH; x++)
         {
-            double xFractal = (x - WIDTH / 2) * 2.0 / WIDTH;
+            double xFractal = (x - WIDTH / 2 - 200) * 2.0 / HEIGHT;
 
             double yFractal = (y - HEIGHT / 2) * 2.0 / HEIGHT;
             int iterations = Mandelbrot::getIterations(xFractal, yFractal);
 
-            uint8_t red = (uint8_t)(256 * (double)iterations / Mandelbrot::MAX_ITERATIONS);
-            bitmap.setPixel(x, y, red, red, red);
-            if (red < min)
-                min = red;
-            if (red > max)
-                max = red;
+            uint8_t color = (uint8_t)(256 * (double)iterations / Mandelbrot::MAX_ITERATIONS);
+            color = color * color * color;
+            bitmap.setPixel(x, y, 0, color, 0);
+            if (color < min)
+                min = color;
+            if (color > max)
+                max = color;
         }
     }
     cout << min << "," << max << endl;
