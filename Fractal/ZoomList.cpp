f9bb517a7c2fa2e30d4fee8f8c0455fbe1c09cb8
dorisxx <dorisxiang@gatech.edu>
2021-04-08 17:40:44 -0400

    add zoom

--- Fractal/ZoomList.cpp
+++ Fractal/ZoomList.cpp
@@ -1,4 +1,6 @@
 #include "ZoomList.h"
+#include <iostream>
+using namespace std;
 
 namespace BitmapAll
 {
@@ -8,9 +10,15 @@ namespace BitmapAll
     void ZoomList::add(const Zoom &zoom)
     {
         zooms.push_back(zoom);
+        m_xCenter += (zoom.x - m_width / 2) * m_scale;
+        m_yCenter += (zoom.y - m_height / 2) * m_scale;
+        m_scale *= zoom.scale;
+        cout << m_xCenter << "," << m_yCenter << "," << m_scale << endl;
     }
     pair<double, double> ZoomList::doZoom(int x, int y)
     {
-        return pair<double, double>(0, 0);
+        double xFractal = (x - m_width / 2) * m_scale + m_xCenter;
+        double yFractal = (y - m_height / 2) * m_scale + m_yCenter;
+        return pair<double, double>(xFractal, yFractal);
     }
 }
