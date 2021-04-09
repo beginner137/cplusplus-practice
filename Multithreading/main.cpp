5c66ad8d5b21b451fbfd2fb3b72c77b4e8683ca4
dorisxx <dorisxiang@gatech.edu>
2021-04-09 19:13:11 -0400

    multithreading

--- Multithreading/main.cpp
+++ Multithreading/main.cpp
@@ -1,30 +1,34 @@
 #include <iostream>
-#include <chrono>
 #include <thread>
 #include <mutex>
+#include <cmath>
+#include <iomanip>
+#include <future>
 
 using namespace std;
 
+double calculatePi(int terms)
+{
+    double sum = 0.0;
+    for (int i = 0; i < terms; i++)
+    {
+        int sign = pow(-1, i);
+        double term = 1.0 / (i * 2 + 1);
+        sum += sign * term;
+    }
+    return sum * 4;
+}
+
 int main()
 {
-    atomic<int> count = 0;
-    const int ITERATIONS = 1e6;
-    mutex mtx;
-    auto lambda = [&]() {
-        for (int i = 0; i < ITERATIONS; i++)
-        {
-            mtx.lock();
-            // critical section, only one thread can access
-            ++count;
-            mtx.unlock();
-        }
+    promise<double> promise;
+    auto do_calculation = [&](int terms) {
+        auto result = calculatePi(terms);
+        promise.set_value(result);
     };
-
-    thread t1(lambda);
-    thread t2(lambda);
-
+    thread t1(do_calculation, 1e7);
+    future<double> future = promise.get_future();
+    cout << setprecision(15) << future.get() << endl;
     t1.join();
-    t2.join();
-    cout << count << endl;
     return 0;
 }
