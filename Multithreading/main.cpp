13a65545a35604800078b88107c9a4f3aa7e6e43
dorisxx <dorisxiang@gatech.edu>
2021-04-09 20:38:28 -0400

    multithread use packaged task

--- Multithreading/main.cpp
+++ Multithreading/main.cpp
@@ -1,54 +1,49 @@
 #include <iostream>
 #include <thread>
-#include <mutex>
+#include <future>
 #include <cmath>
 #include <iomanip>
-#include <future>
 
 using namespace std;
 
-double calculatePi(int terms)
+double calculate_pi(int terms)
 {
+    double sum = 0.0;
+
     if (terms < 1)
     {
         throw runtime_error("Terms cannot be less than 1");
     }
-    double sum = 0.0;
+
     for (int i = 0; i < terms; i++)
     {
         int sign = pow(-1, i);
         double term = 1.0 / (i * 2 + 1);
         sum += sign * term;
     }
+
     return sum * 4;
 }
 
 int main()
 {
-    promise<double> promise;
-    auto do_calculation = [&](int terms) {
-        try
-        {
-            auto result = calculatePi(terms);
-            promise.set_value(result);
-        }
-        catch (const exception &e)
-        {
-            promise.set_exception(current_exception());
-        }
-    };
-    thread t1(do_calculation, 0);
-    future<double> future = promise.get_future();
+    packaged_task<double(int)> task1(calculate_pi);
+
+    future<double> future1 = task1.get_future();
+
+    thread t1(move(task1), 0);
 
     try
     {
-        cout << setprecision(15) << future.get() << endl;
+        double result = future1.get();
+        cout << setprecision(15) << result << endl;
     }
-    catch (const std::exception &e)
+    catch (exception &e)
     {
-        cout << e.what() << '\n';
+        cout << "ERROR! " << e.what() << endl;
     }
 
     t1.join();
+
     return 0;
 }
