dd6805f20db79569296862f11ab60ed5be156d88
dorisxx <dorisxiang@gatech.edu>
2021-04-10 20:42:14 -0400

    a thread pool using blocking queue

--- Multithreading/thread_pool.cpp
+++ Multithreading/thread_pool.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
+#include <future>
+#include <chrono>
 #include <thread>
-#include <queue>
+#include <vector>
 #include <mutex>
-#include <condition_variable>
+#include <cmath>
+#include <queue>
 
 using namespace std;
+
+mutex g_mtx;
 
 template <typename E>
 class blocking_queue
@@ -22,10 +27,12 @@ public:
 
     void push(E e)
     {
-        // ensure thread safety
         unique_lock<mutex> lock(_mtx);
+
         _cond.wait(lock, [this]() { return _queue.size() < _max_size; });
+
         _queue.push(e);
+
         lock.unlock();
         _cond.notify_one();
     }
@@ -34,16 +41,19 @@ public:
     {
         unique_lock<mutex> lock(_mtx);
         _cond.wait(lock, [this]() { return !_queue.empty(); });
+
         return _queue.front();
     }
 
-    E pop()
+    void pop()
     {
         unique_lock<mutex> lock(_mtx);
+
         _cond.wait(lock, [this]() { return !_queue.empty(); });
+
         _queue.pop();
+
         lock.unlock();
-        // unspecified , could be a random thread
         _cond.notify_one();
     }
 
@@ -54,30 +64,41 @@ public:
     }
 };
 
+int work(int id)
+{
+    unique_lock<mutex> lock(g_mtx);
+    cout << "Starting " << id << endl;
+    lock.unlock();
+
+    int seconds = int((5.0 * rand()) / RAND_MAX + 3);
+    this_thread::sleep_for(chrono::seconds(seconds));
+
+    return id;
+}
+
 int main()
 {
-    blocking_queue<int> qu(5);
+    // Here the argument supplied to the queue needs to be
+    // one less than the number of threads you want to launch.
+    blocking_queue<shared_future<int> > futures(2);
 
-    thread t1([&]() {
-        for (int i = 0; i < 10; i++)
+    thread t([&]() {
+        for (int i = 0; i < 20; i++)
         {
-            cout << "pushing" << i << endl;
-            cout << "queue size is  " << qu.size() << endl;
-            qu.push(i);
+            shared_future<int> f = async(launch::async, work, i);
+            futures.push(f);
         }
     });
 
-    thread t2([&]() {
-        for (int i = 0; i < 10; i++)
-        {
-            auto item = qu.front();
-            qu.pop();
-            cout << "consumed " << item << endl;
-        }
-    });
+    for (int i = 0; i < 20; i++)
+    {
+        shared_future<int> f = futures.front();
+        int value = f.get();
+        futures.pop();
+        cout << "Returned: " << value << endl;
+    }
 
-    t1.join();
-    t2.join();
+    t.join();
 
     return 0;
 }
