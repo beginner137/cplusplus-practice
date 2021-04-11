c6369fd16432992dc00c90b94ef89203f45ce164
dorisxx <dorisxiang@gatech.edu>
2021-04-10 20:01:59 -0400

    multithreading thread-safe blocking queue

--- Multithreading/main.cpp
+++ Multithreading/main.cpp
@@ -1,49 +1,83 @@
 #include <iostream>
 #include <thread>
-#include <future>
-#include <cmath>
-#include <iomanip>
+#include <queue>
+#include <mutex>
+#include <condition_variable>
 
 using namespace std;
 
-double calculate_pi(int terms)
+template <typename E>
+class blocking_queue
 {
-    double sum = 0.0;
+private:
+    mutex _mtx;
+    condition_variable _cond;
+    int _max_size;
+    queue<E> _queue;
 
-    if (terms < 1)
+public:
+    blocking_queue(int max_size) : _max_size(max_size)
     {
-        throw runtime_error("Terms cannot be less than 1");
     }
 
-    for (int i = 0; i < terms; i++)
+    void push(E e)
     {
-        int sign = pow(-1, i);
-        double term = 1.0 / (i * 2 + 1);
-        sum += sign * term;
+        // ensure thread safety
+        unique_lock<mutex> lock(_mtx);
+        _cond.wait(lock, [this]() { return _queue.size() < _max_size; });
+        _queue.push(e);
+        lock.unlock();
+        _cond.notify_one();
     }
 
-    return sum * 4;
-}
+    E front()
+    {
+        unique_lock<mutex> lock(_mtx);
+        _cond.wait(lock, [this]() { return !_queue.empty(); });
+        return _queue.front();
+    }
+
+    E pop()
+    {
+        unique_lock<mutex> lock(_mtx);
+        _cond.wait(lock, [this]() { return !_queue.empty(); });
+        _queue.pop();
+        lock.unlock();
+        // unspecified , could be a random thread
+        _cond.notify_one();
+    }
+
+    int size()
+    {
+        lock_guard<mutex> lock(_mtx);
+        return _queue.size();
+    }
+};
 
 int main()
 {
-    packaged_task<double(int)> task1(calculate_pi);
+    blocking_queue<int> qu(5);
 
-    future<double> future1 = task1.get_future();
+    thread t1([&]() {
+        for (int i = 0; i < 10; i++)
+        {
+            cout << "pushing" << i << endl;
+            cout << "queue size is  " << qu.size() << endl;
+            qu.push(i);
+        }
+    });
 
-    thread t1(move(task1), 0);
-
-    try
-    {
-        double result = future1.get();
-        cout << setprecision(15) << result << endl;
-    }
-    catch (exception &e)
-    {
-        cout << "ERROR! " << e.what() << endl;
-    }
+    thread t2([&]() {
+        for (int i = 0; i < 10; i++)
+        {
+            auto item = qu.front();
+            qu.pop();
+            cout << "consumed " << item << endl;
+        }
+    });
 
     t1.join();
+    t2.join();
 
     return 0;
 }
